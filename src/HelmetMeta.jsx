16d954af568e213195aa76f7a90ccbd563ae651b
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-07-01 17:33:54 -0500

    helmet files names changed

--- src/HelmetMeta.jsx
+++ src/HelmetMeta.jsx
@@ -1,15 +1,15 @@
 import { Helmet } from "react-helmet";
-import Resume from "../../../portfolio/src/settings/resume.json";
-import Settings from "../../../portfolio/src/settings/settings.json";
+import resume from "../../../portfolio/src/settings/resume.json";
+import settings from "../../../portfolio/src/settings/settings.json";
 
 export const HelmetMeta = () => {
     return (
         <Helmet>
-            <meta name="theme-color" content={Settings.colors.primary} />
-            <title>{Resume.basics.name} | {Resume.basics.location.city}, {Resume.basics.location.country}</title>
-            <meta name="author" content={Resume.basics.name} />
-            <meta name="description" content={Resume.basics.description} />
-            <meta name="keywords" content={Resume.basics.keywords} />
+            <meta name="theme-color" content={settings.colors.primary} />
+            <title>{resume.basics.name} | {resume.basics.location.city}, {resume.basics.location.country}</title>
+            <meta name="author" content={resume.basics.name} />
+            <meta name="description" content={resume.basics.description} />
+            <meta name="keywords" content={resume.basics.keywords} />
         </Helmet>
     );
 };
