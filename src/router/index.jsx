dc9005d7c634bf5804f1dad7bd2630ff2e5e6ec1
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-11-20 19:06:31 +0100

    page not found route updated

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,11 +1,16 @@
 import { useRoutes } from "react-router-dom";
 import { Home } from "../pages/Home";
+import { PageNotFound } from "../pages/PageNotFound";
 
 function Router() {
     return useRoutes([
       {
         path: "/",
         element: <Home />
+      },
+      {
+        path: "/*",
+        element: <PageNotFound />
       }
     ])
   }
